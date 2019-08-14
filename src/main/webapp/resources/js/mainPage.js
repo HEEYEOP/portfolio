1479aae7d2023581c570a7256b0c0f1cbf461291
gmlduq <gmlduqrhdwn@naver.com>
2019-08-14 16:54:59 +0900

    8/14(수)_스프링_메인페이지 css, js 파일로 뺌

---
+++ src/main/webapp/resources/js/mainPage.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+$(document).ready(function(){
+	//검색버튼 누르면 검색어 입력창 띄우기
+	$('#imgMenuSearch').click(function(){
+	    $('#searchWrap').toggleClass('fold');
+	    $('#searchBox').toggleClass('fold');
+	})
+	//----------------------------------------
+	
+	
+})
